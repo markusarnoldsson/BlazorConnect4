8d04d1b129ec99b04d480b3d88c7f5425076d703
markusarnoldsson <62673521+markusarnoldsson@users.noreply.github.com>
2021-10-04 23:32:24 +0200

    Added test code used to run training in program.cs
    Co-Authored-By: cati19hd <62722541+cati19hd@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -19,8 +19,8 @@ namespace BlazorConnect4
             {
                 Directory.CreateDirectory("./Data");
             }
-            //CreateHostBuilder(args).Build().Run();
-            Training();
+            CreateHostBuilder(args).Build().Run();
+            //Training();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -33,17 +33,26 @@ namespace BlazorConnect4
         {
             Console.WriteLine("Initialize training protocol!");
 
-            AIModels.QAgent EasyAi = new AIModels.QAgent(Model.CellColor.Yellow);
-            AIModels.QAgent randomAi = new AIModels.QAgent(Model.CellColor.Red);
+            //AIModels.QAgent EasyYellowAi = AIModels.QAgent.ConstructFromFile("Data/EasyYellowAi.bin");
+            //AIModels.QAgent EasyRedAi = AIModels.QAgent.ConstructFromFile("Data/EasyRedAi.bin");
+
+            AIModels.QAgent MediumYellowAi = new AIModels.QAgent(Model.CellColor.Yellow);
+            AIModels.QAgent MediumRedAi = new AIModels.QAgent(Model.CellColor.Red);
+            AIModels.RandomAI randomAI = new AIModels.RandomAI();
 
 
             Console.WriteLine("AI's Loaded");
 
-            randomAi.TrainAgent(EasyAi, 1000);
-            //Console.WriteLine("Victories: " + EasyAi.wins + "\n" + "Ties: " + EasyAi.ties + "\n" + "Defeats: " + EasyAi.losses + "\n" + "Games played: " + EasyAi.nrOfGames + "\n");
+            //EasyYellowAi.TrainAgent(EasyRedAi, 10000);
+            //EasyYellowAi.TrainAgent(randomAI, 10000);
+
+            //EasyRedAi.TrainAgent(randomAI, 10000);
+            //EasyRedAi.TrainAgent(EasyYellowAi, 10000);
+
+
             Console.WriteLine("Time for some R&R!");
-            //EasyYellowAi.ToFile("Data/EasyDifficulty.bin");
-            //EasyRedAi
+            //EasyYellowAi.ToFile("Data/EasyYellowAi.bin");
+            //EasyRedAi.ToFile("Data/EasyRedAi.bin");
         }
     }
 }
